20757ec1f4bd40662dec97fa636d5e29563ad1fd
unknown <anirbit16@gmail.com>
2026-07-05 14:08:55 +0530

    Import Images. Create Performance Component and Individual Teacher profile

---
+++ app/dashboard/teacher/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Announcements from "@/components/Announcements";
+
+const SingleTeacherPage = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default SingleTeacherPage
